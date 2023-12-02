157ce27293432c61f45eacb7249b4535e020a348
Lestechocoy <123592065+Lestechocoy@users.noreply.github.com>
2023-12-02 13:37:14 -0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
   }*/
 
    mysql: {
-      host: process.env.MYSQL_HOST || 'https://server0751.hostilmitado.com',
+      host: process.env.MYSQL_HOST || 'https://server0751.hostilimitado.com',
       user: process.env.MYSQL_USER || 'tecnoxum_chocoy',
       password: process.env.MYSQL_PASSWORD || '@Chocoy2023@',
       database: process.env.MYSQL_DB || 'tecnoxum_chocoy'
